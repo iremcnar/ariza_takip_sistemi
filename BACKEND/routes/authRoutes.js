9e81bc8bcd8d3a513fcd80f0b8307dbfa4937013
İrem <41iremcinar@gmail.com>
2025-07-18 08:34:23 +0300

    authRoutes.js açıklamaları

--- BACKEND/routes/authRoutes.js
+++ BACKEND/routes/authRoutes.js
@@ -1,12 +1,18 @@
-const express = require('express');
-const router = express.Router();
-const {
+//kullanıcıyla ilgili kimlik doğrulama ve yetkilendirme işlemlerini yöneten API rotalarını yönetir.
+// Bu dosya, kullanıcı kayıt, giriş ve profil bilgilerini alma gibi işlemleri içerir.
+
+const express = require('express'); //
+const router = express.Router(); // Express.js ile yönlendirme işlemlerini yapar.
+
+const { 
   registerUser,
   loginUser,
   getMe
 } = require('../controllers/authController');
 const { protect } = require('../middleware/auth');
 
+// post isteği ile kullanıcı kaydı, giriş ve profil bilgilerini alma işlemlerini yöneten rotalar tanımlanır.
+// get isteği ile kullanıcı doğrulama işlemleri yapılır.
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/me', protect, getMe);
