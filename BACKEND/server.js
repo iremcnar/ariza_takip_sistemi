c132edbe4153c556e25c27623284b600da49a02a
İrem <41iremcinar@gmail.com>
2025-07-18 08:29:40 +0300

    server.js açıklamaları

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,14 +1,17 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+// Bu dosyanın amacı backend sunucusunu başlatmak ve yapılandırmaktır. (Apı oluşturma)
+// HTTTP isteklerini dinler, MongoDB veritabanına bağlanır ve gerekli rotaları tanımlar.
+// Route(yol) yönetimi, middleware kullanımı ve MongoDB bağlantısı gibi temel işlevleri içerir.
+require('dotenv').config(); //.env dosyasından bilgileri almamıza yarar. Bu sayede gizli bilgileri kodda direkt yazmamış oluruz.
+const express = require('express'); // Web sunucusu oluşturmak için
+const mongoose = require('mongoose'); //MongoDB ile etkileşim için
+const cors = require('cors'); // Farklı kaynaklardan gelen istekleri kabul etmek için
 
 const app = express();
 
 // Middleware
-app.use(cors());
-app.use(express.json());
-app.use('/uploads', express.static('uploads'));
+app.use(cors());  // CORS ayarları, farklı kaynaklardan gelen istekleri kabul eder. Backend ve frontend farklı portlarda çalıştığı için bu gerekli.
+app.use(express.json()); // Gelen JSON formatındaki istek gövdesini (body) işler.
+app.use('/uploads', express.static('uploads')); // 'uploads' klasöründeki dosyaları statik olarak sunar. Örneğin, resim yükleme işlemleri için kullanılır.
 
 // MongoDB Bağlantısı
 mongoose.connect(process.env.MONGODB_URI)
